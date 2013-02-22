fd179320056e67f920d9eb38ed5b82b7c9abfec2
Hakim Bin Luqman <binluqma@polytech.unice.fr>
2013-02-22 14:13:33 +0100

    Completed workerBirths and soldierBirths
    completed the birth() methods for these two classes.

--- src/antColony/SoldierBirths.java
+++ src/antColony/SoldierBirths.java
@@ -1,5 +1,8 @@
 package antColony;
 
+import enums.BirthNumber;
+import enums.EventEnum;
+
 /**
  * Implements the methods for birthing soldiers. Only one instance of this class should be created
  * in each AntColonyBoard.
@@ -11,22 +14,53 @@ public class SoldierBirths extends Births{
 		super(ants);
 	}
 
+	/*  
+	 * Test the number of nurses that the player has. If there are sufficient nurses, reduce the 
+	 * number of Nurses by two (for the first birth) or by one (for the second birth) and increment
+	 * the number of soldiers. If the event affecting the player is the Soldier+1 event, the player
+	 *  gets one bonus soldier for each birth.
+	 */
 	@Override
 	public void birth() {
-		// TODO Auto-generated method stub
-		// refer to LarvaBirths.java
-		
+		switch(birthNumber) {
+		case FIRST:	
+			if(acb.getAnts().getNbNurses() >= 2){
+				takeNurse(2);
+				acb.getAnts().addSoldier();
+				birthNumber=BirthNumber.SECOND;
+
+				if(acb.getEvent().getActiveEvent() == EventEnum.SOLDIER){
+					acb.getAnts().addSoldier();
+				}
+			}
+			else { 
+				System.out.println("\nInsufficient nurses for first soldier birth!");
+			}
+			break;
+
+		case SECOND:
+			if(acb.getAnts().getNbNurses() >= 1){
+				takeNurse(1);
+				acb.getAnts().addSoldier();
+				birthNumber=BirthNumber.THIRD;
+
+				if(acb.getEvent().getActiveEvent() == EventEnum.SOLDIER){
+					acb.getAnts().addSoldier();
+				}
+			}
+			else { 
+				System.out.println("\nInsufficient nurses for second soldier birth!");
+			}
+			break;
+
+		case THIRD:
+			System.out.println("No more soldier births possible!");
+			break;
+
+		case NONE:
+			System.out.println("No more soldier births possible!");
+			break;
+		}	
 	}
 
-	@Override
-	public void takeNurse(int nbNursesToTake) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void returnNurses() {
-		// TODO Auto-generated method stub
-		
-	}
 }
--- src/antColony/WorkerBirths.java
+++ src/antColony/WorkerBirths.java
@@ -1,5 +1,8 @@
 package antColony;
 
+import enums.BirthNumber;
+import enums.EventEnum;
+
 /**
  * Implements the methods for birthing workers. Only one instance of this class should be created
  * in each AntColonyBoard.
@@ -11,21 +14,46 @@ public class WorkerBirths extends Births{
 		super(ants);
 	}
 
+	/*  
+	 * Test the number of nurses that the player has. If there is are at least two nurses, reduce the 
+	 * number of Nurses by two and increment the number of workers. If the event affecting the player is
+	 * the Worker+1 event, the player gets one bonus worker for each birth.
+	 */
 	@Override
 	public void birth() {
-		// TODO Auto-generated method stub
+		if(acb.getAnts().getNbNurses() >= 2){
+			if(acb.getEvent().getActiveEvent() == EventEnum.WORKER && birthNumber != BirthNumber.NONE
+			   && birthNumber != BirthNumber.THIRD){
+				acb.getAnts().addWorker();
+			}
+			switch(birthNumber) {
+			case FIRST:	
+				takeNurse(2);
+				acb.getAnts().addWorker();
+				acb.getAnts().addWorker();
+				birthNumber=BirthNumber.SECOND;
+				break;
 
-	}
+			case SECOND:
+				takeNurse(1);
+				acb.getAnts().addWorker();
+				acb.getAnts().addWorker();
+				birthNumber=BirthNumber.THIRD;
+				break;
 
-	@Override
-	public void takeNurse(int nbNursesToTake) {
-		// TODO Auto-generated method stub
+			case THIRD:
+				System.out.println("No more worker births possible!");
+				break;
 
-	}
+			case NONE:
+				System.out.println("No more worker births possible!");
+				break;
+			}	
+		}
 
-	@Override
-	public void returnNurses() {
-		// TODO Auto-generated method stub
-		
+		else 
+		{ 
+			System.out.println("\nInsufficient nurses for Worker birth!");
+		}
 	}
 }
